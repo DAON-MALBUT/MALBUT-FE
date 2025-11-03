1d60e6d45d9016c10f3ff8807541e9a286bf5303
dohun08 <bemik624@gmail.com>
2025-11-04 00:23:24 +0900

    feat/#1 :: 권한 요청 화면 제작

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import MobileLayout from './layouts/mobile';
 import CharacterList from './pages/character/list';
 import CharacterInit from './pages/character/init';
 import CharacterCreate from './pages/character/create';
+import SplashPage from '@/pages/splash';
+import Permission from './pages/permission';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MobileLayout>Home Page</MobileLayout>} />
+        <Route path="/" element={<SplashPage />} />
         <Route path='/character'>
           <Route index element={<CharacterList />} />
           <Route path='init' element={<CharacterInit />} />
           <Route path='create' element={<CharacterCreate />} />
         </Route>
+        <Route path="/permission" element={<Permission />} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
